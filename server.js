51eca0091352239df092f4c5e2058e7588f9151c
gr632202-del <gr632202@gmail.com>
2026-04-27 20:30:47 +0530

    Update server.js
    Fix server from public folder

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// Ye line important hai - public folder use kar
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
